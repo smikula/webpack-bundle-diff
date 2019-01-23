25d126082ce61a5bf37b6a3968edd7b18d34c2f0
Scott Mikula <mikula@gmail.com>
2019-01-23 09:51:01 -0800

    Include chunk group data in bundle data

--- src/api/deriveBundleData/deriveBundleData.ts
+++ src/api/deriveBundleData/deriveBundleData.ts
@@ -1,9 +1,11 @@
 import { BundleData } from '../../types/BundleData';
 import { Stats } from '../../types/Stats';
 import { deriveGraph } from './deriveGraph';
+import { deriveChunkGroupData } from './deriveChunkGroupData';
 
 export function deriveBundleData(stats: Stats): BundleData {
     return {
         graph: deriveGraph(stats),
+        chunkGroups: deriveChunkGroupData(stats),
     };
 }
