9851724010e7379f3d44591e8fd7dfd92bb8843c
Scott Mikula <smikula@microsoft.com>
2022-05-30 22:23:30 -0700

    Start tests

---
+++ test/api/getStatsFromRawStatsTests.ts
@@ -0,0 +1,6 @@
+describe('getStatsFromRawStats', () => {
+    const stats1 = { name: 'stats1' };
+    const stats2 = { name: 'stats2' };
+
+    it('for just a single stats, returns it', () => {});
+});
